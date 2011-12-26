cbb4ffd1c922817187366518c06caa5f5dd5dbc9
Nadav Samet <thesamet@gmail.com>
2011-12-25 22:19:48 -0800

    refactor dependency building. install all of them to the same directory

--- cmake_modules/install_deps.cmake
+++ cmake_modules/install_deps.cmake
@@ -1,34 +1,33 @@
 include(library_suffix)
 include(ExternalProject)
 
-# Install GFlags
+function(BuildDependency TARGETNAME URL VARNAME LIBNAME)
 ExternalProject_Add(
-    GFlagsLib
-    URL http://google-gflags.googlecode.com/files/gflags-1.7.tar.gz
+    ${TARGETNAME}
+    URL ${URL}
+    INSTALL_DIR ${PROJECT_BINARY_DIR}/deps
     CONFIGURE_COMMAND <SOURCE_DIR>/configure  --prefix=<INSTALL_DIR>
     )
-ExternalProject_Get_Property(GFlagsLib install_dir)
+ExternalProject_Get_Property(${TARGETNAME} install_dir)
 include_directories(${install_dir}/include)
-set(GFLAGS_LIBRARIES ${install_dir}/lib/libgflags.${link_library_suffix})
-set(GFLAGS_PREFIX ${install_dir})
+set(${VARNAME}_LIBRARIES ${install_dir}/lib/${LIBNAME}.${link_library_suffix}
+  PARENT_SCOPE)
+endfunction()
 
-# Install GLog
-ExternalProject_Add(
-    GLogLib
-    URL http://google-glog.googlecode.com/files/glog-0.3.1-1.tar.gz
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --with-gflags=${GFLAGS_PREFIX}
-    )
-ExternalProject_Get_Property(GLogLib install_dir)
-include_directories(${install_dir}/include)
-set(GLOG_LIBRARIES ${install_dir}/lib/libglog.${link_library_suffix})
+BuildDependency(
+    GFlags
+    http://google-gflags.googlecode.com/files/gflags-1.7.tar.gz
+    GFLAGS
+    libgflags)
 
-# Install ZeroMQ
-ExternalProject_Add(
+BuildDependency(
+    GLog
+    http://google-glog.googlecode.com/files/glog-0.3.1-1.tar.gz
+    GLOG
+    libglog)
+
+BuildDependency(
     ZeroMQ
-    URL http://download.zeromq.org/zeromq-2.1.11.tar.gz
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=<INSTALL_DIR> --with-gflags=${GFLAGS_PREFIX}
-    )
-ExternalProject_Get_Property(ZeroMQ install_dir)
-include_directories(${install_dir}/include)
-set(ZEROMQ_LIBRARIES ${install_dir}/lib/libzmq.${link_library_suffix})
-
+    http://download.zeromq.org/zeromq-2.1.11.tar.gz
+    ZEROMQ
+    libzmq)
